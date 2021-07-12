d0955d99fc42e6d654b17b990729529061af0189
victorhgb <victorhugo-brito@hotmail.com>
2021-07-12 19:18:33 -0300

    hr user, oauth

---
+++ hr-user/src/main/java/com/victorhgb/hruser/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.victorhgb.hruser.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victorhgb.hruser.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	User findByEmail(String email);
+}
